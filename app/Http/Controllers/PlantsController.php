b54c312875a7ff4179c19c33c3507d7b48c6c63d
joseareia <jose.apareia@gmail.com>
2021-05-28 18:44:44 +0100

    add(plantsController): new validation and error handling

--- app/Http/Controllers/PlantsController.php
+++ app/Http/Controllers/PlantsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Plant;
 use Illuminate\Http\Request;
+use App\Http\Requests\StorePlantRequest;
+use Illuminate\Support\Facades\Validator;
 
 class PlantsController extends Controller
 {
@@ -15,7 +17,30 @@ class PlantsController extends Controller
 
     public function store(Request $request)
     {
-        $plant = Plant::create($request->all());
+        if ($request->has('name')) {
+            $request->merge(array('name' => strtolower($request->name)));
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|alpha|in:alfaces,tomates,cenouras',
+            'temperature' => 'required|numeric|between:-100,100',
+            'luminosity' => 'required|numeric|between:-100,100',
+            'humidity' => 'required|numeric|between:-100,100',
+            'light' => 'required|boolean',
+            'watering' => 'required|boolean',
+            'photo' => 'required',
+        ],
+        $messages = [
+            'name.in' => 'The selected name is invalid. Valid options are \'alfaces\', \'cenouras\' and \'tomates\'.',
+        ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->all(), 400);
+        }else {
+            $plant = Plant::create($request->all());
+        }
+
         return response()->json($plant, 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\PlantsController;
 
 Route::get('plants', [PlantsController::class, 'index']);
 Route::post('plants', [PlantsController::class, 'store']);
+
+//query for test -> name=cenouras&temperature=32&luminosity=67&humidity=3&light=0&watering=1&photo=webcam.png
