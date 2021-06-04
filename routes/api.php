69dd5ec8305e5e7e66e3cb87e645bc59cf4dc2f5
joseareia <jose.apareia@gmail.com>
2021-06-04 11:31:39 +0100

    add(python): new python script and routes

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlantsController;
+use App\Http\Controllers\WebcamController;
 use App\Http\Controllers\GeneralSensorController;
 
 Route::post('plants', [PlantsController::class, 'store']);
 //usage: name=cenouras&temperature=32&luminosity=67&humidity=3&light=0&watering=1&photo=webcam.png
 Route::post('general-sensor', [GeneralSensorController::class, 'store']);
 //usage: temperature=23&humidity=32
+
+Route::get('face-recognition', [WebcamController::class, 'face_recognition']);
